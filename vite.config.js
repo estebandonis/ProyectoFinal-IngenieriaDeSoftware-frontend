52caa681ae68634ab87e80be63252d131ad42d86
VIL20776 <vil20776@uvg.edu.gt>
2023-05-03 11:04:42 -0600

    Fixed @components route

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
       { find: '@pages', replacement: path.resolve(__dirname, './src/pages') },
       { find: '@store', replacement: path.resolve(__dirname, './src/store') },
       //{ find: '@hooks', replacement: path.resolve(__dirname, './src/hooks') },
-      { find: '@components', replacement: path.resolve(__dirname, './src/assets/Components') },
+      { find: '@components', replacement: path.resolve(__dirname, './src/components') },
     ]
   }
 })
