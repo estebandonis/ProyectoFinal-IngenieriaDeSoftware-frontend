73009dc5120c6dd9124f8893236e47197fb26b23
Esteban Donis <estebandonis10@gmail.com>
2023-05-03 20:22:43 -0600

    componente big picture creado

--- src/components/BigPicture/BigPicture.jsx
+++ src/components/BigPicture/BigPicture.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import MenuIcon from '@mui/icons-material/Menu';
-import { styles } from './BigPicture.module.css'
+import { styles, name } from './BigPicture.module.css'
 
 
-const BigPicture = ({ nombre, tipo, imagen,  rating}) => {
+const BigPicture = ({ nombre, tipo, imagen,  rating, click}) => {
     return (
-        <div className={styles}>
-            <div className="nombre"></div>
-            <div className=""></div>
+        <div className={styles} onClick={click}>
+            <div className={name}>{nombre}</div>
+            <div className="">{tipo}</div>
             <img src={imagen} alt="" />
-            <div className="rating"></div>
+            <div className="rating">{rating}</div>
         </div>
     )
 }
