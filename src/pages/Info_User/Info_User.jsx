6e64e5e7620da4d6d229b492e7d78372b5281e21
VIL20776 <vil20776@uvg.edu.gt>
2023-05-25 17:06:53 -0600

    change password button added

--- src/pages/Info_User/Info_User.jsx
+++ src/pages/Info_User/Info_User.jsx
@@ -3,6 +3,7 @@ import store, { navigate } from '@store'
 import { useStoreon } from 'storeon/react';
 import { styles, info_section, description, informacion, lower_parts, reviews, servicios } from './Info_User.module.css';
 import { Divider, Review, Servicio } from '@components';
+import { Button } from 'react-bootstrap';
 
 const Info_User = () => {
   
@@ -14,8 +15,7 @@ const Info_User = () => {
         <h1>{user.correo}</h1>
         <div className={informacion}>
           <div className={description}>
-            <h2>Informacion General:</h2>
-            <p>{user.isLoggedIn}</p>
+            <Button variant='text' color='inherit'>Cambiar contraseña</Button>
           </div>
 
           <img src="https://chlapaz.files.wordpress.com/2021/01/foto-occidente.jpg" alt="" />
