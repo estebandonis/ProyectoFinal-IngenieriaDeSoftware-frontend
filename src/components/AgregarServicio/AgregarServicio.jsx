8cf5d20d590a91dffbc551c130fca032d764f564
Esteban Donis <estebandonis10@gmail.com>
2023-08-09 17:13:54 -0600

    Agregar servicios de hospitales agregado

--- src/components/AgregarServicio/AgregarServicio.jsx
+++ src/components/AgregarServicio/AgregarServicio.jsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from "react";
 
-import { useApi, useForm } from '@hooks'
+import { useApi } from '@hooks'
 import { styles, info, boton, datos } from './AgregarServicio.module.css'
 
-const AgregarServicio = ({}) => {
+const AgregarServicio = ({ examenOnChange, priceOnChange}) => {
     const { data, handleRequest } = useApi()
 
     const requestNames = async() => {
@@ -15,8 +15,15 @@ const AgregarServicio = ({}) => {
         let ignore = false;
         if (!ignore)  requestNames()
         return () => { ignore = true; }
-      },[]);
-    
+    },[]);
+
+    const examenChange = (examen) => {
+      examenOnChange(examen.target.value)
+    }
+
+    const priceChange = (valor) => {
+      priceOnChange(valor.target.value)
+    }
 
     return (
         <div className={styles}>
@@ -26,15 +33,16 @@ const AgregarServicio = ({}) => {
                 
                 {
                     data!=null?
-                    <select>
+                    <select onChange={examenChange}>
+                      <option value={'Default'} >Default</option>
                     {data.map((name, index) => (
                         <option key={index} value={name.nombre}>{name.nombre}</option>
                     ))}
-                    </select> : <select><option>default</option></select>
+                    </select> : <select><option>Default</option></select>
                 }
                 
               <h2>Precio</h2>
-              <input type="number" />
+              <input type="number" onChange={priceChange}/>
             </div>
           </div>
         </div>
--- src/pages/Add_Hospital/Add_Hospital.jsx
+++ src/pages/Add_Hospital/Add_Hospital.jsx
@@ -19,17 +19,36 @@ const schema = Joi.object({
 })
 
 const Add_Hospital = () => {
-  const { data, handleRequest } = useApi();
-  const form = useForm(schema, { num: '', nombre: '', descripcion: '', direccion: ''})
-  const {dispatch, user } = useStoreon('user')
+  const { handleRequest } = useApi();
+  const form = useForm(schema, { num: '', nombre: '', descripcion: '', direccion: '', zona: ''})
+  const { user } = useStoreon('user')
+
+  const [examenes, setExamenes] = useState({})
+  const [precios, setPrecios] = useState({})
 
   const [numServicios, setNumServicios] = useState(1);
-  const [servicios, setServicios] = useState([<AgregarServicio key={0} />]);
+
+  const examenesOnChange = (key, exam) => {
+    setExamenes(prev => ({...prev, [key]: exam}))
+  }
+
+  const pricesOnChange = (key, precio) => {
+    setPrecios(prev => ({...prev, [key]: precio}))
+  }
+
+  const deletePrice =(prec) => {
+    const updatedPrices = {...precios}; // copy existing state
+    delete updatedPrices[prec]; // delete property
+    console.log("index", prec)
+    setPrecios(updatedPrices); // set new state
+  }
+
+  const [servicios, setServicios] = useState([<AgregarServicio key={0} examenOnChange={(val) => examenesOnChange(0, val)} priceOnChange={(val) => pricesOnChange(0, val)}/>]);
 
   const agregarServicio = () => {
     const newNumServicios = numServicios + 1;
     setNumServicios(newNumServicios);
-    setServicios([...servicios, <AgregarServicio key={newNumServicios - 1} />]);
+    setServicios([...servicios, <AgregarServicio key={newNumServicios - 1} examenOnChange={(val) => examenesOnChange(newNumServicios - 1, val)} priceOnChange={(val) => pricesOnChange(newNumServicios - 1, val)}/>]);
   }
 
   const quitarServicio = (index) => {
@@ -37,28 +56,39 @@ const Add_Hospital = () => {
       const newNumServicios = numServicios - 1;
       setNumServicios(newNumServicios);
       setServicios(servicios.filter((_, i) => i !== index));
+      deletePrice(numServicios - 1);
     }
-  }
-
-  const handleClick = async() => {
-    AddHospital()
-  }
+  }  
 
   const AddHospital = async () => {
     const dpi = form.values.num
     const name = form.values.nombre
     const description = form.values.descripcion
     const direction = form.values.direccion
-
-    console.log("datos", dpi, name, description, direction)
-
-    console.log("id user: ", user)
+    const zone = form.values.zona
   
     // Llama al endpoint de la API para guardar los datos del usuario
-    //const response = await handleRequest('POST', `/users/addUser/${correo}&${contra}`, user);
+    const response = await handleRequest('POST', `/hospitales/addHospital/${name}&${direction}&${description}&${zone}&${user.correo}`);
   
     // Si la respuesta indica que los datos se guardaron correctamente, navega al inicio
-    //console.log("Respuesta: "+response)
+    console.log("Respuesta: " + response)
+
+    const response1 = await handleRequest('PUT', `/users/addDPI/${dpi}&${user.correo}`)
+
+    for (let i = 0; i < numServicios; i++) {
+      const examen = examenes[i]
+      const precio = precios[i]
+
+      const response2 = await handleRequest('POST', `/servicios/addServicio/${examen}&${precio}&${name}`)
+    }
+
+    if (response == true){
+      alert("Hospital agregado exitosamente")
+
+      navigate('/')
+    } else {
+      alert("No se pudo agregar el hospital ingresado")
+    }
   }
 
   return (
@@ -72,11 +102,13 @@ const Add_Hospital = () => {
           <h2>Nombre del Hospital</h2>
           <input type="text" placeholder="Ejemplo: Hospital San Juan de Dios" value={form.values.nombre} onChange={form.onChange('nombre')}/>
           <h2>Descripción del hospital</h2>
-          <textarea name="" id="" cols="30" rows="10"></textarea>
+          <textarea name="" id="" cols="30" rows="10" value={form.values.descripcion} onChange={form.onChange('descripcion')}/>
           <h2>Dirección del hospital</h2>
           <input type="text" placeholder="Mientras más clara sea, más fácil será para los usuarios encontrar el hospital" value={form.values.direccion} onChange={form.onChange('direccion')}/>
+          <h2>Zona</h2>
+          <input type="text" placeholder="Escriba la zona donde se encuentra el hospital, como: 1, 2" value={form.values.zona} onChange={form.onChange('zona')}/>
           <h2>Imágenes</h2>
-          <button onClick={handleClick}>Subir imágenes</button>
+          <button onClick={AddHospital}>Subir imágenes</button>
         </div>
 
         <div className={hospitalInfo}>
@@ -87,7 +119,7 @@ const Add_Hospital = () => {
           </div>
           <div className={allServices}>
             {[...Array(numServicios)].map((_, i) => (
-              <AgregarServicio key={i} />
+              <AgregarServicio key={i} examenOnChange={(val) => examenesOnChange(i, val)} priceOnChange={(val) => pricesOnChange(i, val)}/>
             ))}
           </div>
         </div>
