d6f8e1d0e5cdaecbc5bc63c0ecea805b02a6159d
GonzaloSantizo <san21504@uvg.edu.gt>
2023-08-18 18:47:32 -0600

    Added examenes Filter

--- src/pages/Examenes/Examenes.jsx
+++ src/pages/Examenes/Examenes.jsx
@@ -1,36 +1,76 @@
-import React, { useEffect } from 'react';
-import store, { navigate } from '@store'
+import React, { useEffect, useState } from 'react';
+import { navigate } from '@store';
 import { useStoreon } from 'storeon/react';
-import { useApi } from '@hooks'
+import { content, searchContainer, searchInput } from './Examenes.module.css';
+import { Navbar, Examen } from '@components'; // Import the Examen component
+import { useApi } from '@hooks';
 import { styles, examenes_section } from './Examenes.module.css';
-import { Navbar, Examen } from '@components';
+
+
 
 const Examenes = () => {
-  const { loading, data, handleRequest } = useApi()
-  const { dispatch } = useStoreon('examen')
+  const { loading, data, handleRequest } = useApi();
+  const { user } = useStoreon('user');
+  const { dispatch, examen } = useStoreon('examen'); // Update to use 'examen' store
 
-  const reson = async() => {
-    const response = await handleRequest('GET', '/examenes')
-    return response.data
-  }
+  const reson = async () => {
+    const response = await handleRequest('GET', '/examenes'); // Fetch data from 'examenes'
+    return response.data;
+  };
 
   useEffect(() => {
-    reson()
-  },[]);
+    let ignore = false;
+    if (!ignore) reson();
+    return () => {
+      ignore = true;
+    };
+  }, []);
+
+  // Agregar estado para almacenar el término de búsqueda
+  const [searchTerm, setSearchTerm] = useState('');
+  const [filteredData, setFilteredData] = useState([]);
+
+  // Función para actualizar el término de búsqueda
+  const handleSearchTermChange = event => {
+    setSearchTerm(event.target.value);
+  };
+
+  // Función para buscar examenes que coincidan con el término de búsqueda
+  const handleSearch = () => {
+    const filteredData = data.filter(examen => examen.nombre.toLowerCase().includes(searchTerm.toLowerCase()));
+    setFilteredData(filteredData);
+  };
+
+  useEffect(() => {
+    if (data) {
+      const filteredData = data.filter(examen => examen.nombre.toLowerCase().includes(searchTerm.toLowerCase()));
+      setFilteredData(filteredData);
+    }
+  }, [data, searchTerm]);
+
 
   const handleClick = async (id, nombre, descripcion) => {
-    const nuevo = { examenid: id, nombre: nombre, descripcion: descripcion }
-    dispatch('examen/set', nuevo)
-    navigate('/info_examen')
-  }
+    const nuevo = {
+      examenid: id,
+      nombre: nombre,
+      descripcion: descripcion,
+    };
+    dispatch('examen/set', nuevo); // Update to use 'examen' store
+    console.log('examen: ', examen);
+    navigate('/info_examenes');
+  };
 
   return (
-    <div className={styles}>
-        <Navbar showBackButton={true} />
-        {
+    <div className={content}>
+      <Navbar />
+      <div className={searchContainer}>
+        <input type="text" className={searchInput} placeholder="Buscar" value={searchTerm} onChange={handleSearchTermChange} />
+        
+      </div>
+      {
           data!=null?
           <div className={examenes_section}>
-            {data.map((card, index) => (
+            {filteredData.map((card, index) => (
               <Examen onclick={() => handleClick(card.examenid, card.nombre, card.descripcion)} nombre={card.nombre} url={'https://medlineplus.gov/images/Xray_share.jpg'} />
             ))}
           </div>
