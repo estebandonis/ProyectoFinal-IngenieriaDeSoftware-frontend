21bf51ca9bcbaea5db33a97b3b7db41deb39b242
Esteban Donis <estebandonis10@gmail.com>
2023-09-21 13:25:45 -0600

    Solución de bugs en página info_hospitales

--- src/pages/Info_Hospitales/Info_Hospitales.jsx
+++ src/pages/Info_Hospitales/Info_Hospitales.jsx
@@ -10,7 +10,7 @@ const Info_Hospitales = () => {
   const { data, handleRequest } = useApi()
   const { hospital } = useStoreon('hospital');
   const { user } = useStoreon('user')
-  const [dataReviews, setDataReviews] = useState(null)
+  const [dataReviews, setDataReviews] = useState(false)
   const [averageRating, setAverageRating] = useState(0)
   const [newReview, setNewReview] = useState({
     comentario: '',
@@ -19,12 +19,16 @@ const Info_Hospitales = () => {
 
   const resonServicios = async() => {
     const response = await handleRequest('GET', `/servicios/getServiciosByHospital/${hospital.hospitalid}`)
-    return response.data
+    console.log("SErvicios:", response)
+    console.log(response !== false)
+    return response
   }
 
   const resonReviews = async() => {
     const response = await fetch(`http://localhost:3000/api/v1/reviews/getReviewsByHospital/${hospital.hospitalid}`)
     .then(res => res.json())
+    // console.log("Reviews: ", response)
+    // console.log(response !== false)
     return response
   }
 
@@ -59,15 +63,19 @@ const Info_Hospitales = () => {
   }
 
   const getAverageRating = () => {
-    let rating = 0
-    
-    for (let i = 0; i < dataReviews.length; i++) {
-      rating += parseFloat(dataReviews[i].rating)
+    if (dataReviews !== false) {
+      let rating = 0
+
+      for (let i = 0; i < dataReviews.length; i++) {
+        rating += parseFloat(dataReviews[i].rating)
+      }
+
+      rating = parseFloat(rating / dataReviews.length).toFixed(1)
+
+      setAverageRating(rating)
+    } else {
+        setAverageRating(0)
     }
-
-    rating = parseFloat(rating / dataReviews.length).toFixed(1)
-
-    setAverageRating(rating)
   }
 
   useEffect(() => {
@@ -76,9 +84,7 @@ const Info_Hospitales = () => {
   },[]);
 
   useEffect(() => {
-    if (dataReviews != null) {
-      getAverageRating()
-    }
+    getAverageRating()
   },[dataReviews]);
 
   return (
@@ -93,7 +99,7 @@ const Info_Hospitales = () => {
           </div>
 
           <img src="https://chlapaz.files.wordpress.com/2021/01/foto-occidente.jpg" alt="" />
-        </div>Î
+        </div>
       </div>
 
       <div className={lower_parts}>
@@ -111,27 +117,33 @@ const Info_Hospitales = () => {
                 ))}
           </span>
 
-          <div className={addReview}>
-            <div className={comentario}>
-              <h3>Comentario:</h3>
-              <textarea type="text" id='comentario' value={newReview.comentario} onChange={comentarioOnChange}/>
-            </div>
-            <div className={rating}>
-              <h3>Rating:</h3>
-              <input type="number" id='rating' value={newReview.rating} onChange={comentarioOnChange}/>
-            </div>
-          </div>
+          {
+            user.isLoggedIn?
+            <div>
+              <div className={addReview}>
+                <div className={comentario}>
+                  <h3>Comentario:</h3>
+                  <textarea type="text" id='comentario' value={newReview.comentario} onChange={comentarioOnChange}/>
+                </div>
+                <div className={rating}>
+                  <h3>Rating:</h3>
+                  <input type="number" id='rating' value={newReview.rating} onChange={comentarioOnChange}/>
+                </div>
+              </div>
 
-          <button type="submit" onClick={handleClick}>Subir Review</button>
+              <button type="submit" onClick={handleClick}>Subir Review</button>
+            </div>
+            :<h2>Inicia sesión o crea una cuenta para comentar</h2>
+          }
 
           {
-            dataReviews!=null?
+            dataReviews !== false && dataReviews !== null?
             <div className={serviciosScroll}>
               {dataReviews.map((card, index) => (
                 <Review key={index} nombre={card.correo} comentario={card.comentario} rating={parseInt(card.rating)}/>
               ))}
             </div>
-            :<h2>Cargando...</h2>
+            :<h2>Se el primero en comentar</h2>
           }
         </div>
 
@@ -140,7 +152,7 @@ const Info_Hospitales = () => {
         <div className={servicios}>
           <h1>Servicios</h1>
           {
-            data!=null?
+            data !== false && data !== null?
             <div className={serviciosScroll}>
               {data.map((card, index) => (
                 <Servicio index={index} examen={card.nombre} precio={card.precio}/>
