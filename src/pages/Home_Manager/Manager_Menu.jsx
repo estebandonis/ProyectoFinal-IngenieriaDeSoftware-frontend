54868f8a035bf19488458613dceabea55a9d1eeb
VIL20776 <vil20776@uvg.edu.gt>
2023-10-10 23:35:13 -0600

    Manager_Menu.jsx: Html and function

--- src/pages/Home_Manager/Manager_Menu.jsx
+++ src/pages/Home_Manager/Manager_Menu.jsx
@@ -2,9 +2,13 @@ import React from "react";
 
 const Manager_Menu = () => {
 
+    const fetchReviews = () => {
+
+    }
+
     return (
         <div>
-            
+            <h3>Comentarios registrados</h3>
         </div>
     )
 }
