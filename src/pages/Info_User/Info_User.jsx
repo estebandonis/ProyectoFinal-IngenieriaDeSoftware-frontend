e87781b929fbc7a7c8392610e098fa4162baa6fd
AGM54 <134453750+AGM54@users.noreply.github.com>
2023-10-12 16:00:37 -0600

    Update Info_User.jsx

--- src/pages/Info_User/Info_User.jsx
+++ src/pages/Info_User/Info_User.jsx
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
+import store, { navigate } from '@store'
 import { useStoreon } from 'storeon/react';
 import { styles, info_section, description, informacion, lower_parts, reviews, servicios } from './Info_User.module.css';
 import { Navbar } from '@components';
-import { Button } from 'react-bootstrap';
+import { Dropdown, DropdownButton, Button } from 'react-bootstrap';
 import { useApi, useForm } from '@hooks'
 import Joi from 'joi';
-import { useEffect } from 'react';
 
 const Info_User = () => {
   const schema = Joi.object({
@@ -15,59 +15,64 @@ const Info_User = () => {
         .required(),
   })
 
-  const [changePasswordMenu, setChangePasswordMenu] = useState(false)
-
+  const [changePasswordMenu, setChangePasswordMenu] = useState(false);
+  const [showEditMenu, setShowEditMenu] = useState(false);
   const { data, handleRequest } = useApi();
   const form = useForm(schema, { old_password: '', new_password: ''})
   const { user } = useStoreon('user');
 
   const handlePasswordMenu = () => {
-    setChangePasswordMenu(!changePasswordMenu)
+    setChangePasswordMenu(!changePasswordMenu);
+  }
+
+  const toggleEditMenu = () => {
+    setShowEditMenu(!showEditMenu);
   }
 
   const respond = async() => {
     const response = await handleRequest('PUT', `/users/updateUserPassword/${user.correo}&${form.values.old_password}&${form.values.new_password}`)
-    return response
+    return response;
   }
 
   const handleClick = async() => {
-    const response = await respond()
-    form.values.old_password = ''
-    form.values.new_password = ''
-    setChangePasswordMenu(false)
+    const response = await respond();
+    form.values.old_password = '';
+    form.values.new_password = '';
+    setChangePasswordMenu(false);
   }
-
-  useEffect(() => {
-    if (data != null) {
-      alert(data)
-    }
-  }, [data])
 
   return (
     <div className={styles}>
       <Navbar showBackButton={true}/>
+
+      <DropdownButton id="dropdown-item-button" title="Perfil">
+        <Dropdown.Item as="button" onClick={handlePasswordMenu}>Cambiar contraseña</Dropdown.Item>
+        <Dropdown.Item as="button" onClick={toggleEditMenu}>Editar perfil</Dropdown.Item>
+      </DropdownButton>
+
       <div className={info_section}>
         <h1>{user.correo}</h1>
         <div className={informacion}>
           <div className={description}>
-            <Button variant='text' color='inherit' onClick={handlePasswordMenu}>Cambiar contraseña</Button>
-            <div>
-              {
-                changePasswordMenu && (
-                <form>
-                  <label>Contraseña original:</label><br></br>
-                  <input type='password' value={form.values.old_password} onChange={form.onChange('old_password')}></input><br></br>
-                  <label>Nueva contraseña:</label><br></br>
-                  <input type='password' value={form.values.new_password} onChange={form.onChange('new_password')}></input>
-                  <br></br>
-                  <br></br>
-                  <Button variant='text' color='inherit' onClick={handleClick}>Aceptar</Button>
-                </form> )
-              }
-            </div>
-            
-          </div>
+            { showEditMenu && (
+              <>
+                <h2>Editar Perfil</h2>
+                <p>Funcionalidad aún no implementada</p>
+              </>
+            )}
 
+            {
+              changePasswordMenu && (
+              <form>
+                <label>Contraseña original:</label><br />
+                <input type='password' value={form.values.old_password} onChange={form.onChange('old_password')}></input><br />
+                <label>Nueva contraseña:</label><br />
+                <input type='password' value={form.values.new_password} onChange={form.onChange('new_password')}></input>
+                <br />
+                <Button variant='text' color='inherit' onClick={handleClick}>Aceptar</Button>
+              </form> )
+            }
+          </div>
           <img src="https://chlapaz.files.wordpress.com/2021/01/foto-occidente.jpg" alt="" />
         </div>
       </div>
