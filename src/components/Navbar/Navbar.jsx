180a35156b0b1e8387182abd29ac4093d4b710d0
Esteban Donis <estebandonis10@gmail.com>
2023-11-06 16:23:57 -0600

    Mejoras en texto de navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import { AppBar, Toolbar, IconButton, Typography, Button, Menu, MenuItem } from "@mui/material";
+import { AppBar, Toolbar, IconButton, Typography, Button, Menu, MenuItem, Box } from "@mui/material";
 import React, { useState } from "react";
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import MenuIcon from '@mui/icons-material/Menu';
@@ -61,28 +61,34 @@ const Navbar = ({ showBackButton }) => {
                             <ArrowBackIcon />
                         </IconButton>
                     )}
-                    {user.isLoggedIn ? 
-                        <Button className={estilos.stylesButton} onClick={handleMenuClick} variant="text" color="inherit">{user.correo}</Button> : 
-                        <Button className={estilos.stylesButton} onClick={handleMenuClick} variant="text" color="inherit"> Perfil </Button>
-                    }
-                    <Menu
-                        anchorEl={anchorEl}
-                        open={Boolean(anchorEl)}
-                        onClose={handleMenuClose}
-                    >
-                        {user.isLoggedIn ? null : <MenuItem onClick={handleLoginClick}>Ingresar</MenuItem>}
-                        {user.isLoggedIn ? <MenuItem onClick={handleUserClick}>Ver perfil</MenuItem> : null}
-                        {user.isLoggedIn ? <MenuItem onClick={handleLogoutClick}>Cerrar sesión</MenuItem> : null} 
-                        {user.isLoggedIn ? <MenuItem onClick={handleAddHospitalClick}>Agregar hospital</MenuItem> : null} 
-                        {user.isLoggedIn && user.tipo === 'manager' ? <MenuItem onClick={handleUpdateHospitalClick}>Actualizar hospital</MenuItem> : null}
-                        {user.isLoggedIn && user.tipo === 'manager' ? <MenuItem onClick={handleHospitalReviewsClick}>Reseñas de hospitales</MenuItem> : null}
-                    </Menu>
-                    <Typography variant="h5" flexGrow={1}>
-                        MedicEasy
-                    </Typography>
-                    <Button className={estilos.stylesButton} variant="text" color="inherit" onClick={handleExamenesClick}>
-                        Exámenes
-                    </Button>
+                    <Box display="flex" justifyContent="start" width="33%">
+                        {user.isLoggedIn ? 
+                            <Button className={estilos.stylesButton} onClick={handleMenuClick} variant="text" color="inherit" >{user.correo}</Button> : 
+                            <Button className={estilos.stylesButton} onClick={handleMenuClick} variant="text" color="inherit"> Perfil </Button>
+                        }
+                        <Menu
+                            anchorEl={anchorEl}
+                            open={Boolean(anchorEl)}
+                            onClose={handleMenuClose}
+                        >
+                            {user.isLoggedIn ? null : <MenuItem onClick={handleLoginClick}>Ingresar</MenuItem>}
+                            {user.isLoggedIn ? <MenuItem onClick={handleUserClick}>Ver perfil</MenuItem> : null}
+                            {user.isLoggedIn ? <MenuItem onClick={handleLogoutClick}>Cerrar sesión</MenuItem> : null} 
+                            {user.isLoggedIn ? <MenuItem onClick={handleAddHospitalClick}>Agregar hospital</MenuItem> : null} 
+                            {user.isLoggedIn && user.tipo === 'manager' ? <MenuItem onClick={handleUpdateHospitalClick}>Actualizar hospital</MenuItem> : null}
+                            {user.isLoggedIn && user.tipo === 'manager' ? <MenuItem onClick={handleHospitalReviewsClick}>Reseñas de hospitales</MenuItem> : null}
+                        </Menu>
+                    </Box>
+                    <Box display="flex" justifyContent="center" flexBasis="33%">
+                        <Typography variant="h5">
+                            MedicEasy
+                        </Typography>
+                    </Box>
+                    <Box display="flex" justifyContent="end" flexBasis="33%">
+                        <Button className={estilos.stylesButton} variant="text" color="inherit" onClick={handleExamenesClick}>
+                            Exámenes
+                        </Button>
+                    </Box>
                 </Toolbar>
             </AppBar>
         </div>
