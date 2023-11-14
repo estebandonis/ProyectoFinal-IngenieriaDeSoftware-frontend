63495dceee581a2b8f258e8ad6e438959bec451f
Esteban Donis <estebandonis10@gmail.com>
2023-11-14 17:43:05 -0600

    Mejoras en componente examen

--- src/components/Examen/Examen.jsx
+++ src/components/Examen/Examen.jsx
@@ -5,10 +5,10 @@ import { styles } from './Examen.module.css'
 const Examen = ({ onclick, nombre, url }) => {
 
     return (
-        <button onClick={onclick} className={styles}>
+        <div onClick={onclick} className={styles}>
             <img src={url} alt="" />
             <h3>{nombre}</h3>
-        </button>
+        </div>
     )
 }
 
